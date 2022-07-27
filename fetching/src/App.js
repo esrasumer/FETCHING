af8b556c213e95e3c972bf8b596f11cc7d49d940
esrasumer <esrasumer28@gmail.com>
2022-07-27 11:12:49 +0300

    fetch and axios

---
+++ fetching/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import  Users from './components/Users'
+
+function App() {
+  return (
+    <div className="App">
+     <Users />
+     <h3>native fetch: herhangibir veri kaynağına gidip o veri kaynağındaki 
+    ilgili veriyi aldıktan sonra ekranımızda nasıl göstereceğiz ve fetch ile 
+    yapacaklarımızı axios ile de yapabiliriz</h3>
+    
+    </div>
+  );
+}
+
+export default App;
